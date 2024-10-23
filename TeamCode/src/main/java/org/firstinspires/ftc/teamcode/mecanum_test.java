de952f2de0d35958b94263b2f98ebaf7b49a15ed
coeng <coen.gentry@gmail.com>
2024-10-22 19:33:29 -0600

    fix AUTO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum_test.java
@@ -16,6 +16,10 @@ public class mecanum_test extends LinearOpMode {
     boolean touchpadwpressed = false;
     boolean clawclose = false;
     boolean clawopen = false;
+    boolean hangup = false;
+    boolean hangdown = false;
+    boolean unloaded = false;
+    boolean loaded = false;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -26,7 +30,9 @@ public class mecanum_test extends LinearOpMode {
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
         Servo clawServo = hardwareMap.servo.get("clawServo");
+        Servo primeServo = hardwareMap.servo.get("primeServo");
         DcMotor armMotor1 = hardwareMap.dcMotor.get("armMotor1");
+        DcMotor hangmotor = hardwareMap.dcMotor.get("hangmotor");
 
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
@@ -37,6 +43,7 @@ public class mecanum_test extends LinearOpMode {
         backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         armMotor1.setDirection(DcMotorSimple.Direction.REVERSE);
+        hangmotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
 
         waitForStart();
@@ -49,6 +56,10 @@ public class mecanum_test extends LinearOpMode {
             double rx = gamepad1.right_stick_x;
             clawclose = gamepad1.right_bumper;
             clawopen = gamepad1.left_bumper;
+            loaded = gamepad1.b;//Set these differently
+            unloaded = gamepad1.a;//Set these differently
+            hangup = gamepad1.dpad_up;
+            hangdown = gamepad1.dpad_down;
             touchpadpressed = gamepad1.touchpad;
             if (touchpadpressed && ! touchpadwpressed) {
                 slowmode = ! slowmode;
@@ -64,12 +75,7 @@ public class mecanum_test extends LinearOpMode {
             double backLeftPower = ((y + x + rx) / denominator) * multiplier;
             double frontRightPower = ((y - x - rx) / denominator) * multiplier;
             double backRightPower = ((y + x - rx) / denominator) * multiplier;
-            if (clawopen) {
-                setpose(clawServo,120);//2/3 of what is wanted EX: 180 degrees wanted use 120
-            }
-           if (clawclose) {
-               setpose(clawServo, 0);
-           }
+
            double armpower = 0;
            if (gamepad1.right_trigger > 0) {
                armpower = gamepad1.right_trigger;
@@ -78,12 +84,30 @@ public class mecanum_test extends LinearOpMode {
            }
             armpower = Range.clip(armpower, -0.5,0.5);
 
+           double hangpower = 0;
+           if (gamepad1.dpad_up) {
+               hangpower = 1;
+           } else if (gamepad1.dpad_down) {
+               hangpower = -1;
+           }
+//           double clawpower = 0.5;
+           if (gamepad1.right_bumper) {
+               setpose(clawServo, 110);   // closing degree (grip force)
+           }   else if (gamepad1.left_bumper) {
+               setpose(clawServo,  60);  // opening degree keep positive otherwise will hit edges
+            }
+           if (gamepad1.b) {
+               setpose(primeServo, 110);//change degrees.
+           } else if (gamepad1.a) {
+               setpose(primeServo, 60);//change degrees
+           }
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
             armMotor1.setPower(armpower);
+            hangmotor.setPower(hangpower);
             touchpadwpressed = touchpadpressed;
             telemetry.addData("slowmode",slowmode);
             telemetry.update();
