ec7f8f73f198fc57ce9844c51ab91f7e207cb529
coeng <coen.gentry@gmail.com>
2025-01-09 14:19:35 -0700

    fixed mm

--- MeepMeepMod/src/main/java/com/example/meepmeepmod/MeepMeepTesting.java
+++ MeepMeepMod/src/main/java/com/example/meepmeepmod/MeepMeepTesting.java
@@ -21,16 +21,30 @@ public class MeepMeepTesting {
 
         myBot.runAction(myBot.getDrive().actionBuilder(startpose)
                 .splineTo(new Vector2d(0,-34),Math.toRadians(90.0))
+                //Insert the code for top speci hang
+
+                //start next movement
                 .setTangent(Math.toRadians(270))
                 .splineToConstantHeading(new Vector2d(-48,-39),Math.toRadians(90.0))
+                //insert code for the pickup, transfer, and deposit
+
+                //start next movement
                 .splineToLinearHeading(new Pose2d(-56,-56,Math.toRadians(225.0)),Math.toRadians(225.0))
-                .setTangent(Math.toRadians(90))
-                .splineTo(new Vector2d(-57,-39),Math.toRadians(-45.0))
+                //insert code for pickup, transfer, and deposit
+
+                //start next movement
+                .turnTo(Math.toRadians(90.0))
+                .strafeToConstantHeading(new Vector2d(-57,-39))
+                //set the robot ready for the next steps(claw up and open)
+
+//                .setTangent(Math.toRadians(90))
+//                .splineTo(new Vector2d(-57,-39),Math.toRadians(-45.0))
 
 
 
                 .splineToLinearHeading(new Pose2d(-56,-56,Math.toRadians(225.0)),Math.toRadians(225.0))
-//
+                .turnTo(Math.toRadians(90))
+                .strafeTo(new Vector2d(56,-56))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
