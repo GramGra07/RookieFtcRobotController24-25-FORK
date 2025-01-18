8a5882ac4bb18b00483f1326a2289bc00d0c0c05
coeng <coen.gentry@gmail.com>
2025-01-18 10:56:22 -0700

    fixed mm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/ServoUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/ServoUtil.java
@@ -4,14 +4,14 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class ServoUtil {
-    public static double clawopen = 0;//good
-    public static double clawclose = 90;//good
+    public static double clawopen = 20;//good
+    public static double clawclose = 200;//good
     public static double primetop = 118;//good
     public static double primemiddle = 57.5;//good
     public static double primelow = 100;
     public static double primebottom = 0;//good
     public static double upclawopen = 200;//good
-    public static double upclawclose = 60;//good
+    public static double upclawclose = 100;//good
     public static double hangtop = 170;//no
     public static double hangtop1 = 170;//good
     public static double hangmiddle = 105;//good
